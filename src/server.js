08266ac123c4b1177a45036119bfb8eddae00831
Igor Ribeiro Sobral <igorsobral@brq.com>
2023-01-16 22:45:54 -0300

    consuming a full stream

--- src/server.js
+++ src/server.js
@@ -4,9 +4,22 @@ import http from 'node:http'
 // JSON - Javascript Object Notation
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
-  
+  console.log(req, 'req')
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
   if(method === 'GET' && url === '/users'){
     return res
       .setHeader('Content-type', 'application/json')  
@@ -14,10 +27,12 @@ const server = http.createServer((req, res) => {
   }
   
   if(method === 'POST' && url === '/users'){
+    const { name, email } = req.body;
+
     users.push({
       id: 1,
-      name: 'Jhon doe',
-      email: 'jonhdoe@examples.com'
+      name,
+      email
     })
 
     return res.writeHead(201).end()
--- stream/fake-upload-to-http-stream.js
+++ stream/fake-upload-to-http-stream.js
@@ -7,17 +7,21 @@ class OneToHundredStream extends Readable{
     const i = this.index++
 
     setTimeout(() => {
-      if(i > 100){
+      if(i > 5){
         this.push(null)
       } else {
         const buff = Buffer.from(String(i));
         this.push(buff)
       }
     }, 1000)
+
   }
 }
 
 fetch('http://localhost:3334', {
   method: 'POST',
   body: new OneToHundredStream(),
-})
+}).then(response => {
+   return response.text()
+}).then(data => console.log(data))
+  .catch(rejected => console.log(rejected))
