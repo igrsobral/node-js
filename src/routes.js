7d2eeadb90297095e6f86e99580f3d33ec7a94b6
igorWebMASTER <igor_1917@hotmail.com>
2023-01-23 21:06:40 -0300

    feat: delete user by id

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
 
@@ -15,7 +16,7 @@ export const routes = [
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body;
 
@@ -28,6 +29,18 @@ export const routes = [
       database.insert('users', user)
 
       return res.writeHead(201).end()
+    },
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      console.log(id)
+
+      database.delete('users', id)
+
+      return res.writeHead(204).end()
     },
   }
 ] 
--- src/server.js
+++ src/server.js
@@ -3,16 +3,24 @@ import http from 'node:http'
 import { json } from './middlewares/json.js'
 import { routes } from './routes.js'
 
+// Query parameters // Stateful ==> Filtros, paginação, não-obrigatórios
+// Route parameters // identificação de recurso
+// Request body: 
+
+
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+    req.params = {...routeParams.groups }
+
     return route.handler(req, res)
   }
   
